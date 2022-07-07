5f4821f0e02b806e77bc2a7d0726cf460017126b
Andrey Stoletov <stoletov314@gmail.com>
2022-07-07 03:44:49 +0200

    Most important song at the human history !

---
+++ youtube.py
@@ -0,0 +1,17 @@
+from pytube import YouTube
+import os
+
+link = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+direction = 'Users/andrey/Downloads'
+
+
+def downloadYouTube(video_link, direction):
+
+    yt = YouTube(video_link)
+    yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+    if not os.path.exists(direction):
+        os.makedirs(direction)
+    yt.download(direction)
+
+
+downloadYouTube(link, direction)
